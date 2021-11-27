5bb32db757e48c402b9399eca075f5b91a6bbf34
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2021-11-27 01:03:44 -0300

    teste calculo vf

--- teste.js
+++ teste.js
@@ -1,5 +1,8 @@
-if (99 == "99") {
-    console.log("O numero é igual a string")
-} else {
-    console.log("O numero não é igual")
+function valorFuturo(valorPresente, taxaJuros, prazoMaturação) {
+    //VF = VP (1 + i)^N
+    var vf = valorPresente * (1 + (taxaJuros/100)) ^ prazoMaturação;
+    return vf;
 }
+
+
+console.log(valorFuturo(1000, 7.5, 12));
