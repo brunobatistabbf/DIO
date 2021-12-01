86225bf3f90c88ac6936602bfa0985bc0246ca7c
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2021-12-01 16:47:49 -0300

    update

--- batalhanaval.js
+++ batalhanaval.js
@@ -1,6 +1,6 @@
 var view = {
     displayMessage: function (msg) {
-        var messageArea = document.getElementById("messageArea");
+        var messageArea = document.getElementById("tap, tap");
         messageArea.innerHTML = msg;
     },
     displayHit: function (location) {
@@ -10,7 +10,23 @@ var view = {
     displayMiss: function (location) {
         var cell = document.getElementById(location);
         cell.setAttribute("class", "miss");
+
     },
 
+
+}
+var model = {
+ 
+    boardSize: 7,
+    numShips: 3,
+    shipLenght: 3,
+    shipsSunk: 0,
+ 
+ 
+ 
+    ships = [{ locations: ["10", "20", "30"], hits: ["", "", ""] },
+    { locations: ["32", "33", "34"], hits: [" ", " ", " "] },
+    { locations: ["63", "64", "65"], hits: ["", "", ""] }
+    ]
 
 }
