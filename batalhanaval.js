11f5e05b1986eb689ae93673dd16685ee66352ea
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2021-12-01 18:40:34 -0300

    update methods

--- batalhanaval.js
+++ batalhanaval.js
@@ -16,17 +16,32 @@ var view = {
 
 }
 var model = {
- 
+
     boardSize: 7,
     numShips: 3,
     shipLenght: 3,
     shipsSunk: 0,
- 
- 
- 
-    ships = [{ locations: ["10", "20", "30"], hits: ["", "", ""] },
+
+
+
+    ships =[{ locations: ["10", "20", "30"], hits: ["", "", ""] },
     { locations: ["32", "33", "34"], hits: [" ", " ", " "] },
     { locations: ["63", "64", "65"], hits: ["", "", ""] }
-    ]
+    ],
+
+    fire: function (guess) {
+        for (var i = 0; i < this.numShips; i++)
+        var ship = this.ships[i];
+        locarions = ship.locations;
+        var index = locations.indexOf(guess);
+        if (index >= 0 ) {
+            console.log("Voce tem um hit");
+            ship.hits[index] = "hit"
+            return true;
+        }
+        return false;
+    }
+    
 
 }
+
